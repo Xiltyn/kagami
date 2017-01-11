5c52f75857009c7127238e58d313acdea698c503
Xiltyn <xiltyn@gmail.com>
2017-01-11 01:41:46 +0100

    dist test commit

--- conf/webpack.conf.js
+++ conf/webpack.conf.js
@@ -38,6 +38,10 @@ module.exports = {
           'react-hot',
           'babel'
         ]
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
       }
     ]
   },
--- src/app/menuBar.js
+++ src/app/menuBar.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 
+const Isvg = require('react-inlinesvg');
+
 export class MenuBar extends Component {
   render() {
     return (
       <div className="kg-menubar">
-        <Logo/>
-        <Nav/>
+        <div className="kg-menubar--logo">
+          <Logo/>
+        </div>
+        <div className="kg-menubar--nav">
+          <Nav/>
+        </div>
       </div>
     );
   }
@@ -14,7 +20,11 @@ export class MenuBar extends Component {
 class Logo extends Component {
   render() {
     return (
-      <img src="images/logo.png"/>
+      <div className="kg-logo ">
+        <div className="kg-logo--medium">
+          <Isvg src="images/svg/logo--medium.svg"/>
+        </div>
+      </div>
     );
   }
 }
@@ -22,8 +32,12 @@ class Nav extends Component {
   render() {
     return (
       <div className="kg-nav">
-        <img src="images/btn_profile.png" className="kg-btn kg-btn--profile"/>
-        <img src="images/btn_login.png" className="kg-btn kg-btn--login"/>
+        <div className="kg-btn">
+          <Isvg src="images/svg/icn-user--white.svg" className="kg-btn--profile"/>
+        </div>
+        <div className="kg-btn">
+          <Isvg src="images/svg/icn-logout--white.svg" className="kg-btn--profile"/>
+        </div>
       </div>
     );
   }
